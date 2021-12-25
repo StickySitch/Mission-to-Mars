776bbc8d209d763d936c670a5ef9afba67f72037
Jack Jones <fuzzydunlop19@gmail.com>
2021-12-25 01:10:56 -0800

    Turned all scraoers into functions.

--- scraping.py
+++ scraping.py
@@ -3,48 +3,61 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
 
+def scrape_all():
+    # Initiate headless driver for deployment
+    exePath = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **exePath, headless=True)
 
-# Set up Splinter
-exePath = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **exePath, headless=False)
+    newsTitle, newsParagraph = mars_news(browser)
+
+    # Run all scraping functions and store results in a dictionary
+    data = {
+        "news_title": newsTitle,
+        "news_paragraph": newsParagraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+    # Stop webdriver and return data
+    browser.quit()
+    return data
+
 
 def mars_news(browser):
 
     # Visit mars NASA news site
-    url = 'https://redplanetscience.com'
+    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
     browser.visit(url)
     # Adding delay for loading page
     browser.is_element_present_by_css('div.list_text', wait_time=1)
 
     # Convert the browser html to a soup object and then quit the browser
     html = browser.html
-    newsSoup = soup(html, 'html.parser'
+    newsSoup = soup(html, 'html.parser')
 
     # Add try/except for error handling
     try:
         slideElem = newsSoup.select_one('div.list_text')
-        #slideElem.find('div',class_='content_title')
 
 
         # Use the parent element to find the first `a` tag and save it as `news_title`
         newsTitle = slideElem.find('div',class_='content_title').get_text()
-        #newsTitle
 
 
         # Use the parent element to find the paragraph text
         newsP = slideElem.find('div', class_='article_teaser_body').get_text()
-        #newsP
+
     except AttributeError:
-        return None,None
+        return None, None
+
     return newsTitle, newsP
 
-# Jet Propulsion Lab
-exePath = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **exePath, headless=False)
+
 def featured_image(browser):
     # Visit URL
-    url  = 'https://spaceimages-mars.com/'
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
     browser.visit(url)
 
 
@@ -64,7 +77,7 @@ def featured_image(browser):
         return None
 
     # Use the base URl to create an absolute URL
-    imgURL = f'https://spaceimages-mars.com/{imgUrlRel}'
+    imgURL = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{imgUrlRel}'
 
 
     return imgURL
@@ -72,17 +85,18 @@ def featured_image(browser):
 
 def mars_facts():
     try:
-        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+        df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
     except BaseException:
         return None
-    return df.to_html()
-    df.columns = ['description', 'Mars', 'Earth']
-    df.set_index('description', inplace=True)
 
-    return df.to_html()
-    #browser.quit()
+    df.columns = ['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
 
+    return df.to_html(classes='table table-striped')
 
 
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
 
 
